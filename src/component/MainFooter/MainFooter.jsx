2a5f7238ca842f7492f54ac0011f1872582b72a2
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2024-03-16 21:14:17 +0530

    create-main-footer-page

---
+++ src/component/MainFooter/MainFooter.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class MainFooter extends Component {
+  render() {
+    return (
+      <View>
+        <Text>MainFooter</Text>
+      </View>
+    )
+  }
+}
